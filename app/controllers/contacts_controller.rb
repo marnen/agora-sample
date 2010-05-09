740a1e4c0bde52f90f08ec580f900cdb491b3cc7
Marnen Laibow-Koser <marnen@marnen.org>
2010-05-08 22:20:34 -0400

    Improve display of errors and flash messages.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     if @contact.save
+      flash[:notice] = 'Contact was successfully created!'
       redirect_to :action => 'index' and return
     else
+      flash[:error] = 'That contact could not be created. Please try again.'
       render :action => 'new'
     end
   end
